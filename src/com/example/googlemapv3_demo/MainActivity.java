85a98e1a736e0aa3fc9778772922c2aa7def2ce9
kevin <kevin@naturaltel.com>
2017-10-12 17:25:58 +0800

    完成webview 交互

--- src/com/example/googlemapv3_demo/MainActivity.java
+++ src/com/example/googlemapv3_demo/MainActivity.java
@@ -1,35 +1,93 @@
 package com.example.googlemapv3_demo;
 
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+	
+	public WebView webView;
+	
+	//-預設座標
+	public String lat = "22.549193";
+	public String lon = "114.071583";
+	
+	//-預設座標陣列
+	public ArrayList<Item> marr;
+	public String[][] a = {
+            {"A", "22.548802", "114.067747"},
+            {"B", "22.544526","114.083781"},
+            {"C", "22.5483557", "114.063093"},
+            {"D", "22.535056", "114.068064"},
+            {"E", "22.547366", "114.066340"},
+            {"F", "22.542912", "114.057919"},
+            {"G", "22.540552", "114.071669"},
+            {"H", "22.543446", "114.067849"}};
+	@SuppressLint({"JavascriptInterface"})
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		webView = (WebView)findViewById(R.id.webView1);
+		webView.getSettings().setJavaScriptEnabled(true);
+		
+		
+		marr =new ArrayList<>();
+		for (int i = 0; i < a.length; i++) {
+			Item item = new Item();
+			item.title = a[i][0];
+			item.lat = a[i][1];
+			item.lon = a[i][2];
+			marr.add(item);
+		}
+		
+		webView.setWebViewClient(new WebViewClient(){
+			//-onPageStarted(網頁開始載入)
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+			}
+			//--onPageFinished(網頁載入完畢)
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				//-此處載入起點座標
+				Log.i("載入", "載入");
+				webView.loadUrl("javascript:initmap('"+ lat + "','"+ lon+"')"); 
+				//webView.loadUrl("javascript:callFromActivity('"+ lat + "','"+ lon+"')");
+				//--傳入列表資料
+				String str = new Gson().toJson(marr);
+				webView.loadUrl("javascript:setMarker('" + str + "')");	
+			}
+		});
+		
+		webView.setWebChromeClient(new WebChromeClient());
+		
+		
+		webView.loadUrl("file:///android_asset/Map3.html");//webView.loadUrl("file:///android_asset/text.html");
+		
 	}
 
 	
 	///===============================
-	/*
-	 zoomControl 啟用/停用「縮放」控制項。此控制項預設會顯示在靠近地圖的右下角。zoomControlOptions 欄位額外指定 ZoomControlOptions 供此控制項使用。
-	 mapTypeControl 啟用/停用讓使用者切換地圖類型（例如「地圖」和「衛星」）的「地圖類型」控制項。此控制項預設會顯示在靠近地圖的左上角。mapTypeControlOptions 欄位額外指定 MapTypeControlOptions 供此控制項使用。
-	 streetViewControl 啟用/停用讓使用者啟用「街景服務」全景功能的「小黃人」控制項。此控制項預設會顯示在靠近地圖的右下角。streetViewControlOptions 欄位額外指定 StreetViewControlOptions 供此控制項使用。
-	 rotateControl 啟用/停用顯示控制 45° 影像方向的「旋轉」控制項。此控制項是否出現預設是由指定地圖類型在目前的縮放和地點是否有 45° 影像來決定。您可以設定地圖的 rotateControlOptions 指定要使用的 RotateControlOptions 來改變控制項的行為。如果目前沒有 45° 影像，就無法讓此控制項顯示。
-	 scaleControl 啟用/停用提供簡單地圖比例的「比例」控制項。此控制項預設不會顯示。如果啟用，則一律顯示在地圖的右下角。scaleControlOptions 額外指定 ScaleControlOptions 供此控制項使用。
-	 fullscreenControl 可啟用/停用以全螢幕模式開啟地圖的控制項。在預設情況下，此控制項在行動裝置上是可見的，但在電腦上則不可見。啟用時，此控制項會出現在地圖右上角附近。fullscreenControlOptions 會額外指定 FullscreenControlControlOptions 以針對此控制項使用。
-	 
-	 
-	 
-	 
-	 
-	 
-	 */
+	class Item {
+		public String title;
+		public String lat;
+		public String lon;
+	}
 	
 }
